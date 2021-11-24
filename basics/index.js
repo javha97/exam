75b950d83bf980c701c4ce112ae89b7d00360805
javha97 <jjavkhlan123@gmail.com>
2021-11-24 12:29:38 +0800

    exam

--- basics/index.js
+++ basics/index.js
@@ -1,10 +1,18 @@
 // 1. 0 - ээс 100 хүртэлх тоог хэвлэ.
 // -- Энд кодоо бичнэ үү!!
+for(let i=1; i<=100; i++){
+    console.log(i)
+}
 
 
 // 2. 60 - с 30 хүртэлх бүх СОНДГОЙ тоог хэвлэ.
 // -- Энд кодоо бичнэ үү!!
-
+for(let i=60; i>30; i--)
+{
+    if(i%2!=0){
+     console.log(i)
+    }
+}
 // 3. Чагтаас бүрдсэн квадрат зурах ба баруун тийшээ налсан диагональ нь нэмэх тэмдэг байна:
 // ######+
 // #####+#
@@ -15,6 +23,9 @@
 // +######
 // -- Энд кодоо бичнэ үү!!
 
+
+
+
 // 4. Регистрийн дугаар өгөхөд төрсөн он, сар, өдөр (YYYY-MM-DD) хүйсийг хэвлэдэг convertRegNum функц бич.
 //      РД - гийн сүүлийн орон сондгой бол эрэгтэй, тэгш бол эмэгтэй байна.
 // Жишээ нь:
@@ -22,14 +33,46 @@
 //      >> 1990-02-03 онд төрсөн эрэгтэй
 // -- Энд кодоо бичнэ үү!!
 
+ 
+function convertRegNum(){
+     let $RegNum='AB90020321' 
+     
+      if($RegNum.charAt(9)%2!=0>0){
+
+          console.log('eregte')
+      }else{
+          console.log('emegte')
+      }
+   
+}
+convertRegNum()
+
+
 // 5. Бүх .red-boxes гэсэн класстай элемэнтийг улаан болго;
 // -- Энд кодоо бичнэ үү!!
 
+// let a=document.querySelectorAll(".red-boxes box").style.color='red'
+
+var elements = document.getElementsByClassName("red-boxes box"); 
+for(var i = 0; i < elements.length; i++){
+    elements[i].style.background = "red";
+}
+
+
+  
+
+
 // 6. blue-box гэсэн ID-тай элемэнтийг цэнхэр болго;
 // -- Энд кодоо бичнэ үү!!
+document.getElementById("blue-box").style.background='blue'
 
 // 7. Бүх div - ийг бор болго;
 // -- Энд кодоо бичнэ үү!!
+var $div=document.getElementsByTagName("div");
+for(let i=0; i<$div.length; i++){
+    $div[i].style.background="brown"
+}
+
 
 // 8. Шинээр box гэсэн класстай section tag үүсгээд, хар өнгөтэй болгоно.
 //    Дотор нь box гэсэн класстай article tag үүсгэнэ.
@@ -37,3 +80,9 @@
 //    section tag - ыг body руу append хийнэ.
 //    улаан бөөрөнхий дээр дарахад бөөрөнхийг ногоон болгоно.
 // -- Энд кодоо бичнэ үү!!
+
+
+
+
+
+
--- datatypes/index.js
+++ datatypes/index.js
@@ -2,17 +2,23 @@ const string = 'apple, banana, kiwi';
 
 // 1. Өгсөн string - ийн бүх үсгийг том болго: 'APPLE, BANANA, KIWI'
 // -- Энд кодоо бичнэ үү!
+let a='ApplE, Banana, kiwi'
+console.log(a.toUpperCase())
 
 // 2. Өгсөн string - ийг дараах хэлбэртэй болго: 'Apple; Banana; Kiwi'
 // -- Энд кодоо бичнэ үү!
+console.log(a.split(',').join(";"))
+
 
 const array = ['d', 'e', 'c', 'a', 'b'];
 // 3. Өгсөн array - ыг үсгийн дарааллаар эрэмблэ.
 // -- Энд кодоо бичнэ үү!
+console.log(array.sort())
 
 // 4. Өгсөн array - ын голын 3н элемэнтийг салгаж аваад array1 - гэсэн хувьсагчид хийнэ.
 //    array болон array1 ийг хэвлэ.
 // -- Энд кодоо бичнэ үү!
+
 
 // 5. Өгсөн array - ын эхний элемэнтийг устага
 // -- Энд кодоо бичнэ үү!
